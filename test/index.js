a3ed12e0dae256d4fb460b2f3cbfde81eeea8e58
ben <beneaves01@hotmail.com>
2019-05-12 19:00:44 +0800

    init

---
+++ test/index.js
@@ -0,0 +1,42 @@
+const crypto = require('crypto'),
+wp = require('../');
+
+(function(){
+
+  let count = 0,
+  nodeWP = crypto.createHash('whirlpool').update('testing').digest('hex'),
+  browserWPSync = wp.encSync('testing');
+
+  console.log('Nodejs whirlpool:\n' + nodeWP + '\n')
+
+  wp.enc('testing', function(err, res){
+    console.log('Browser whirlpool callback:\n' + res + '\n')
+    if(nodeWP === res){
+      console.log('whirlpool callback test pass' + '\n')
+      count++
+    } else {
+      console.log('whirlpool callback test fail' + '\n')
+    }
+  });
+
+  console.log('Browser whirlpool sync:\n' + browserWPSync + '\n')
+
+  if(nodeWP === browserWPSync){
+    console.log('whirlpool sync test pass' + '\n')
+    count++
+  } else {
+    console.log('whirlpool sync test fail' + '\n')
+  }
+
+
+  wp.encP('testing').then(function(res){
+    console.log('Browser whirlpool promise:\n' + res + '\n')
+    console.log('whirlpool promise test pass' + '\n')
+    count++
+
+  }).catch(function(err){
+    console.log('whirlpool promise test fail' + '\n')
+  }).then(function(){
+    console.log('\ntest complete. ' + count + 'x test pass, ' + (3 - count) +'x test fail')
+  })
+})()
